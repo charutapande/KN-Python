25ee0717816d3210e2b620733371b8755d5e7ef8
Kendric <181333067+kendricscoles@users.noreply.github.com>
2025-04-30 10:51:42 +0200

    Create room_type.py

---
+++ model/room_type.py
@@ -0,0 +1,19 @@
+class RoomType:
+    def __init__(self, room_type_id: int, description: str, max_guests: int):
+        if not room_type_id or not isinstance(room_type_id, int):
+            raise ValueError("room_type_id must be an integer")
+        if not description or not isinstance(description, str):
+            raise ValueError("description must be a string")
+        if not isinstance(max_guests, int):
+            raise ValueError("max_guests must be an integer")
+
+        self.__room_type_id = room_type_id
+        self.__description = description
+        self.__max_guests = max_guests
+
+    @property
+    def room_type_id(self): return self.__room_type_id
+    @property
+    def description(self): return self.__description
+    @property
+    def max_guests(self): return self.__max_guests
